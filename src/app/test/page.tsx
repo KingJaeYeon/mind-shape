20c3faf26c4e1d48b16a689f8044175667a731c6
GuGura <wodus331@gmail.com>
2023-11-20 16:34:30 +0900

    commit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Page.propTypes = {
+
+};
+
+function Page() {
+    return (
+        <div>test page</div>
+    );
+}
+
+export default Page;
