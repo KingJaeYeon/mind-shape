868069f1d764c74718b61536f9cff0de771b2d70
pjy <wodus331@vanillacode.net>
2023-11-28 10:28:23 +0900

    hover toolbar

--- src/components/editor/plugins/hover-toolbar.tsx
+++ src/components/editor/plugins/hover-toolbar.tsx
@@ -5,15 +5,24 @@ import { Editor as SlateEditor, Range } from "slate";
 import { Portal } from "@/components/editor/Portal";
 import { cn } from "@/utils/twmarge";
 import { Button } from "@/components/editor/button";
-import { BlockEditor } from "@/components/editor/plugins/custom-editor-plugins";
-import { BLOCK_HEADING_TWO } from "@/constant/slate";
+import {
+  BlockEditor,
+  MarkEditor,
+} from "@/components/editor/plugins/custom-editor-plugins";
+import {
+  BLOCK_HEADING_ONE,
+  BLOCK_HEADING_TWO,
+  MARK_BOLD,
+  MARK_CODE,
+  MARK_ITALIC,
+  MARK_UNDERLINE,
+} from "@/constant/slate";
 
 interface BaseProps {
   className: string;
   [key: string]: unknown;
 }
 
-type OrNull<T> = T | null;
 export default function HoverToolbar() {
   const ref = useRef<HTMLDivElement>(null);
   const editor = useSlate();
@@ -37,11 +46,9 @@ export default function HoverToolbar() {
 
     const domSelection = window.getSelection();
     if (!domSelection) return;
-    console.log("domSelection", domSelection);
     const domRange = domSelection.getRangeAt(0);
-    console.log("domRange", domRange);
     const rect = domRange.getBoundingClientRect();
-    console.log("rect", rect);
+
     el.style.opacity = "1";
     el.style.top = `${rect.top + window.pageYOffset - el.offsetHeight}px`;
     el.style.left = `${
@@ -51,31 +58,69 @@ export default function HoverToolbar() {
 
   return (
     <Portal>
-      {/*<Menu*/}
-      {/*  ref={ref}*/}
-      {/*  // className={"z-1 absolute p-[8px_7px_6px_0px]"}*/}
-      {/*  onMouseDown={(e: any) => {*/}
-      {/*    // prevent toolbar from taking focus away from editor*/}
-      {/*    e.preventDefault();*/}
-      {/*  }}*/}
-      {/*>*/}
-      {/*<Button*/}
-      {/*  onclickHandler={() => {*/}
-      {/*    BlockEditor.toggleBlock(editor, BLOCK_HEADING_TWO);*/}
-      {/*  }}*/}
-      {/*  className={`flex border border-gray-300 px-1.5 py-0.5 italic`}*/}
-      {/*>*/}
-      {/*  H2*/}
-      {/*</Button>*/}
-      {/*<Button*/}
-      {/*  onclickHandler={() => {*/}
-      {/*    BlockEditor.toggleBlock(editor, BLOCK_HEADING_TWO);*/}
-      {/*  }}*/}
-      {/*  className={`flex border border-gray-300 px-1.5 py-0.5 italic`}*/}
-      {/*>*/}
-      {/*  H2*/}
-      {/*</Button>*/}
-      {/*</Menu>*/}
+      <Menu
+        ref={ref}
+        className={
+          "z-1 absolute flex rounded-[4px] bg-[#222] p-[8px_7px_6px] text-white opacity-0"
+        }
+        style={{
+          transition: "opacity 0.75s",
+        }}
+        onMouseDown={(e: any) => {
+          // prevent toolbar from taking focus away from editor
+          e.preventDefault();
+        }}
+      >
+        <Button
+          title={`ctrl+b`}
+          onclickHandler={() => {
+            MarkEditor.toggleMark(editor, MARK_BOLD);
+          }}
+          className={`flex px-1.5 py-0.5 italic`}
+        >
+          B
+        </Button>
+        <Button
+          onclickHandler={() => {
+            MarkEditor.toggleMark(editor, MARK_CODE);
+          }}
+          className={`flex px-1.5 py-0.5 italic`}
+        >
+          C
+        </Button>
+        <Button
+          onclickHandler={() => {
+            MarkEditor.toggleMark(editor, MARK_UNDERLINE);
+          }}
+          className={`flex px-1.5 py-0.5 italic`}
+        >
+          U
+        </Button>
+        <Button
+          onclickHandler={() => {
+            MarkEditor.toggleMark(editor, MARK_ITALIC);
+          }}
+          className={`flex px-1.5 py-0.5 italic`}
+        >
+          I
+        </Button>
+        <Button
+          onclickHandler={() => {
+            BlockEditor.toggleBlock(editor, BLOCK_HEADING_ONE);
+          }}
+          className={`flex px-1.5 py-0.5 italic`}
+        >
+          H1
+        </Button>
+        <Button
+          onclickHandler={() => {
+            BlockEditor.toggleBlock(editor, BLOCK_HEADING_TWO);
+          }}
+          className={`flex px-1.5 py-0.5 italic`}
+        >
+          H2
+        </Button>
+      </Menu>
     </Portal>
   );
 }
