2315e12aff7d13e1a87cf9d051033ad8288e34dc
pjy <wodus331@gmail.com>
2023-11-29 14:44:20 +0900

    hover toolbar fix

--- src/components/shared/Toolbar.tsx
+++ src/components/shared/Toolbar.tsx
@@ -1,15 +1,38 @@
 "use client";
+//Only use in Toolbar.Root
 import * as Toolbar from "@radix-ui/react-toolbar";
 import React from "react";
 import { cn } from "@/utils/twmarge";
+import {
+  SeparatorProps,
+  ToggleGroupProps,
+  ToggleItemProps,
+} from "@/types/toggleType";
 
-type ToggleItemProps = {
-  children: React.ReactNode;
-  value: string;
-  className?: string;
-  ariaLabel?: string;
-  onClick?: () => void;
-};
+export function ToggleGroup(props: ToggleGroupProps) {
+  if (props.type === "single") {
+    return (
+      <Toolbar.ToggleGroup
+        type={props.type}
+        aria-label={props.ariaLabel}
+        defaultValue={props.defaultValue}
+        className={cn("flex gap-1", props.className)}
+      >
+        {props.children}
+      </Toolbar.ToggleGroup>
+    );
+  }
+  return (
+    <Toolbar.ToggleGroup
+      type={props.type}
+      aria-label={props.ariaLabel}
+      className={cn("flex gap-1", props.className)}
+    >
+      {props.children}
+    </Toolbar.ToggleGroup>
+  );
+}
+
 export function ToggleItem(props: ToggleItemProps) {
   return (
     <Toolbar.ToggleItem
@@ -23,5 +46,16 @@ export function ToggleItem(props: ToggleItemProps) {
     >
       {props.children}
     </Toolbar.ToggleItem>
+  );
+}
+
+export function ToggleSeparator(props: SeparatorProps) {
+  return (
+    <Toolbar.Separator
+      className={cn(
+        "mx-[4px] h-[24px] w-[1px] bg-[rgba(255,255,255,.2)]",
+        props.className,
+      )}
+    />
   );
 }
